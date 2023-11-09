1b27f8e298eeeee913ed8b5ecd0949fa9e8c67b9
ujkey <135115849+ujkey@users.noreply.github.com>
2023-11-09 15:16:04 +0900

    Resolves: #2 address, company 타입 구체화

--- src/types.tsx
+++ src/types.tsx
@@ -12,12 +12,27 @@ export interface Todo {
     userId: number;
 }
 
+interface Address {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: { lat: string, lng: string };
+}
+
+interface Company {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+}
+
 export interface User {
     id: number;
     name: string;
+    username: string;
     email: string;
-    address: any[]; //TODO: address interface
+    address: Address[];
     phone: string;
     website: string;
-    company: any[]; //TODO: company interface
+    company: Company[];
 }
